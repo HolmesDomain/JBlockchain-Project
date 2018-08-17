a145c84f3757c664fad74a3941ad72f92c9408eb
Andrew J. Holmes <2pock72@gmail.com>
2018-08-17 11:32:46 -0400

    Add files via upload

--- JBlockTransact.java
+++ JBlockTransact.java
@@ -33,6 +33,68 @@ public class JBlockTransact {
 		return StringUtil.applySha256(
 			StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) +
 			Float.toString(value) + sequence);
+			
 	}
+	
+	public static byte[] applyECDSASig(PrivateKey privateKey, PublicKey publicKey, String input) {
+		
+		Signature dsa;
+		byte[] output = new byte[0];
+		
+		try {
+			
+			dsa = Signature.getInstance("ECDSA","BC");
+			dsa.initSign(privateKey);
+			byte[] strByte = input.getBytes();
+			dsa.update(strByte);
+			byte[] realSig = dsa.sign();
+			output = realSig;
+			
+		} catch(Exception e) {
+			
+			throw new RuntimeException(e);			
+		}
+		return output;
+	}
+	
+	//Verifies a String signature
+	public static boolean verifyECDSASig(PublicKey publicKey, String data, byte[] signature) {
+		
+		try {
+			
+			Signature ecdsaVerify = Signature.getInstance("ECDSA","BC");
+			ecdsaVerify.initVerify(publicKey);
+			ecdsaVerify.update(data.getBytes());
+			return ecdsaVerify.verify(signature);
+			
+		} catch(Exception e) {
+			
+			throw new RuntimeException(e);
+			
+		}
+		
+	}
+	
+	public static String getStringFromKey(Key key) {
+		
+		return Base64.getEncoder().encodeToString(key.getEncoded());
+		
+	}
+	
+	//Signs all the data we dont wish to be tampered with.
+	public void generateSignature(PrivateKey privateKey) {
+		
+	String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+	signature = StringUtil.applyECDSASig(privateKey,data);	
+	
+	}
+	//Verifies the data we signed hasnt been tampered with
+	public boolean verifiySignature() {
+		
+	String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+	return StringUtil.verifyECDSASig(sender, data, signature);
+	
+	}
+	
 }
 
