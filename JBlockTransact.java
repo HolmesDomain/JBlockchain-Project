f475f08fd32f2622eb1530354ee853d122a6ac10
Andrew J. Holmes <2pock72@gmail.com>
2018-08-22 11:46:32 -0400

    update

--- JBlockTransact.java
+++ JBlockTransact.java
@@ -96,5 +96,83 @@ public class JBlockTransact {
 	
 	}
 	
+	//Returns true if new transaction could be created.	
+	public boolean processTransaction() {
+	
+	if(verifiySignature() == false) {
+		System.out.println("#Transaction Signature failed to verify");
+		return false;
+	}
+			
+	//gather transaction inputs (Make sure they are unspent):
+	for(TransactionInput i : inputs) {
+		i.UTXO = NoobChain.UTXOs.get(i.transactionOutputId);
+	}
+
+	//check if transaction is valid:
+	if(getInputsValue() < NoobChain.minimumTransaction) {
+		System.out.println("#Transaction Inputs to small: " + getInputsValue());
+		return false;
+	}
+	
+	//generate transaction outputs:
+	float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+	transactionId = calulateHash();
+	outputs.add(new TransactionOutput( this.reciepient, value,transactionId)); //send value to recipient
+	outputs.add(new TransactionOutput( this.sender, leftOver,transactionId)); //send the left over 'change' back to sender		
+			
+	//add outputs to Unspent list
+	for(TransactionOutput o : outputs) {
+		NoobChain.UTXOs.put(o.id , o);
+	}
+	
+	//remove transaction inputs from UTXO lists as spent:
+	for(TransactionInput i : inputs) {
+		if(i.UTXO == null) continue; //if Transaction can't be found skip it
+		NoobChain.UTXOs.remove(i.UTXO.id);
+	}
+	
+	return true;
+	}
+	
+	//returns sum of inputs(UTXOs) values
+		public float getInputsValue() {
+			float total = 0;
+			for(TransactionInput i : inputs) {
+				if(i.UTXO == null) continue; //if Transaction can't be found skip it 
+				total += i.UTXO.value;
+			}
+			return total;
+		}
+
+	//returns sum of outputs:
+		public float getOutputsValue() {
+			float total = 0;
+			for(TransactionOutput o : outputs) {
+				total += o.value;
+			}
+			return total;
+		}
+	
+	public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+		int count = transactions.size();
+		ArrayList<String> previousTreeLayer = new ArrayList<String>();
+		for(Transaction transaction : transactions) {
+			previousTreeLayer.add(transaction.transactionId);
+		}
+		ArrayList<String> treeLayer = previousTreeLayer;
+		
+		while(count > 1) {
+			treeLayer = new ArrayList<String>();
+			for(int i = 1; i < previousTreeLayer.size();i++) {
+				treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+			}
+			count = treeLayer.size();
+			previousTreeLayer = treeLayer;
+		}
+		String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+		return merkleRoot;
+	}
+	
 }
 
