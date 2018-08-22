8c99acfc8ca4a2ad488b70a4dc6f50f4802d60f9
Andrew J. Holmes <2pock72@gmail.com>
2018-08-22 11:39:11 -0400

    Update and rename Jblokchain.java to JStringUtil.java

--- JStringUtil.java
+++ JStringUtil.java
@@ -55,42 +55,7 @@ Test Implementation
 }
 **/
 
-
-
-public class Block {
-
-	public String hash;
-	public String previousHash;
-	private String data; //meta data
-	private long timeStamp; //as number of milliseconds since 1/1/1970
-	
-	//Block constructor
-	public Block(String data, String previousHash) {
-		
-		this.data = data;
-		this.previousHash = previousHash;
-		this.timeStamp = new Date().getTime();
-		this.hash = calculateHash(); // do this last
-		
-	}
-	
-	public String calculateHash() {
-		String calculatedhash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data );
-		return calculatedhash;
-	}
-	
-	public void mineBlock(int difficulty) {	
-		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
-		
-		while(!hash.substring( 0, difficulty).equals(target)) {
-			nonce ++;
-			hash = calculateHash();
-		}
-		System.out.println("Block Mined!!! : " + hash);
-	}
-}
-
-public class StringUtil {
+public class JStringUtil {
 	
 	public static String applySha256(String input) {
 		try {
